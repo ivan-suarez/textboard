a1fbe52d46b1e3b44148ecb27fa650181733a1ca
ivan-suarez <heyitsivan@protonmail.com>
2020-11-28 12:03:45 +0100

    Initialized project

---
+++ contracts/Textboard.sol
@@ -0,0 +1,24 @@
+pragma solidity 0.5.16;
+
+contract Textboard {
+
+	struct Post{
+		uint id;
+		string text;
+		string date;
+	}
+
+	mapping(uint=>Post) public posts;
+	
+	uint public postCount;
+
+	function addPost(string memory _text, string memory _date) public{
+		postCount++;
+		posts[postCount]=Post(postCount, _text, _date);
+	}
+
+	constructor() public {
+		addPost("Hello world","0");
+		addPost("Whatup","0");
+	}
+}
---
+++ src/js/app.js
@@ -0,0 +1,98 @@
+App = {
+  web3Provider: null,
+  contracts: {},
+  account: '0x0',
+
+  init: function() {
+    return App.initWeb3();
+  },
+
+  initWeb3: function() {
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
+    return App.initContract();
+  },
+
+  initContract: function() {
+    $.getJSON("Textboard.json", function(textboard) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Textboard= TruffleContract(textboard);
+      // Connect provider to interact with contract
+      App.contracts.Textboard.setProvider(App.web3Provider);
+
+      return App.render();
+    });
+  },
+
+  render: function() {
+    var textboardInstance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    content.hide();
+
+    // Load account data
+    web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Your Account: " + account);
+      }
+    });
+
+    // Load contract data
+    App.contracts.Textboard.deployed().then(function(instance) {
+      textboardInstance= instance;
+      return textboardInstance.postCount();
+    }).then(function(postCount) {
+      var postResults= $("#postResults");
+      postResults.empty();
+
+      for (var i = 1; i <= postCount; i++) {
+        textboardInstance.posts(i).then(function(post) {
+          var id = post[0];
+          var text= post[1];
+          var date= post[2];
+
+          // Render candidate Result
+          var postTemplate= "<tr><th>" + id + "</th><td>" + text+ "</td><td>" + date+ "</td></tr>";
+        // var postTemplate = "<p>sketit"+id+";"+text+";"+date+"</p>"; 
+	 postResults.append(postTemplate);
+        });
+      }
+
+      loader.hide();
+      content.show();
+    }).catch(function(error) {
+      console.warn(error);
+    });
+  },
+	post: function(){
+	//	var newPost = "this is a new post";
+//		var newPost = $('#postText');
+		var newPost = document.getElementById("postText").value;
+		var d= new Date();
+		var d8 = d.getDate()+"/"+(d.getMonth()+1)+"/"+d.getFullYear()+" "+d.getHours()+":"+d.getMinutes()+":"+d.getSeconds();
+		console.log(d8);
+		var n = d.getTime();
+		console.log(newPost);
+		App.contracts.Textboard.deployed().then(function(instance){
+	return instance.addPost(newPost, d8);
+//	$("#content").hide();
+//	$("#loader").show();
+	});
+	}
+};
+
+$(function() {
+  $(window).load(function() {
+    App.init();
+  });
+});
