3dc4c1f406c5184777e1eb48b052b8558e46dd30
ivan-suarez <heyitsivan@protonmail.com>
2020-12-10 18:36:51 +0100

    Modified blockchain config

--- truffle.js
+++ truffle.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -6,6 +8,30 @@ module.exports = {
       host: "192.168.1.37", //"127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
+    },
+	  mainnet: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://mainnet.infura.io/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      confirmations: 2,
+      network_id: 1
+    },
+
+	  kovan: {
+      provider: function() {
+        return new HDWalletProvider(
+          process.env.MNEMONIC,
+          `https://kovan.infura.io/${process.env.INFURA_API_KEY}`
+        )
+      },
+      gas: 5000000,
+      gasPrice: 25000000000,
+      network_id: 42
     }
   }
 };
